75670e6bb9cbcf08fac78ddfa12f77c7df319101
zubastu <79757684+zubastu@users.noreply.github.com>
2024-02-20 22:21:15 +0300

    Добавил React-hook-forms

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App/App';
 import './assets/styles/index.scss';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
