cd73f4491b774da766649dd0e6712960356105d8
zubastu <79757684+zubastu@users.noreply.github.com>
2024-02-20 14:28:46 +0300

    Подготовка окружения для разработки, установка библиотек, добавление переменных для цветов и типографии, добавил спорный favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import './index.css'
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App/App';
+import './assets/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
