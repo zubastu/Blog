82e392a31eaa427807fa41f53439ee646d1431d2
zubastu <79757684+zubastu@users.noreply.github.com>
2024-02-20 20:00:51 +0300

    Создал константы для текстового содержимого основного блока, добавил роутер, роутинг

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import { Route, Routes } from 'react-router';
 
 const App = () => {
-
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path="/" element={<></>}>
+        <Route path="topic/:id" element={<></>} />
+      </Route>
+    </Routes>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/App/App';
 import './assets/styles/index.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
