cf74acb0bdbf970d48e0d1e18e80cb9da88d8c5e
zubastu <79757684+zubastu@users.noreply.github.com>
2024-02-22 23:45:58 +0300

    Добавил иконки

--- src/components/Reactions/Reactions.tsx
+++ src/components/Reactions/Reactions.tsx
@@ -1,7 +1,16 @@
 import styles from './styles.module.scss';
+import likeActive from '../../assets/images/active_like.svg';
+import likeDisabled from '../../assets/images/disabled_like.svg';
+import dislikeActive from '../../assets/images/active_dislike.svg';
+import dislikeDisabled from '../../assets/images/disbled_dislike.svg';
 
 const Reactions = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.reaction}></button>
+      <button className={styles.reaction}></button>
+    </div>
+  );
 };
 
 export default Reactions;
