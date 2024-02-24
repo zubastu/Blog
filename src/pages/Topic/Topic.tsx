29061ac22c053195945871166c4aced072d53183
zubastu <79757684+zubastu@users.noreply.github.com>
2024-02-24 11:55:22 +0300

    Верстка топика в подробном виде

--- src/pages/Topic/Topic.tsx
+++ src/pages/Topic/Topic.tsx
@@ -1,7 +1,36 @@
 import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
+import { topicBackLinkTextContent } from '../../constants/textContentConstants';
+import Reactions from '../../components/Reactions/Reactions';
+import { getRandomImage, getRandomNumber } from '../../utils/utils';
 
 const Topic = () => {
-  return <section></section>;
+  return (
+    <section className={styles.topicContainer}>
+      <div className={styles.container}>
+        <Link to="/" className={styles.backLink}>
+          {topicBackLinkTextContent}
+        </Link>
+        <Reactions
+          dislikesCount={getRandomNumber(0, 99)}
+          isDisliked={false}
+          isLiked={false}
+          likesCount={getRandomNumber(0, 999)}
+        />
+      </div>
+
+      <div className={styles.contentContainer}>
+        <h2 className={styles.title}></h2>
+        <img
+          className={styles.image}
+          src={getRandomImage()}
+          alt="Картинка топика"
+        />
+
+        <p className={styles.textContent}></p>
+      </div>
+    </section>
+  );
 };
 
 export default Topic;
